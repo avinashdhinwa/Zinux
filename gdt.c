3f59b9e86ae2d648fc7e4f670a72cdaccaa0b60d
Milos Zivlak <zivlakmilos@gmail.com>
2017-05-15 19:15:05 +0200

    Fix: Loading GDT crash system

--- gdt.c
+++ gdt.c
@@ -1,16 +1,20 @@
 #include "gdt.h"
 
 gdt_t GDT[GDT_ENTRY_COUNT];
+gdt_ptr_t pGDT;
 
-void GDT_create(gdt_t *gdt, uint32_t base, uint32_t limit, uint8_t type)
+void GDT_flush();
+
+void GDT_create(gdt_t *gdt, uint32_t base, uint32_t limit, uint8_t access, uint8_t flags)
 {
     gdt->baseLo = base & 0xFFFF;
     gdt->baseHi = (base >> 16) & 0xFF;
     gdt->baseExt = (base >> 24) & 0xFF;
 
-    gdt->limitLo = base & 0xFFFF;
-    gdt->flagsLimitHi = (base >> 16) & 0x0F;
-    gdt->type = type;
+    gdt->limitLo = limit & 0xFFFF;
+    gdt->flagsLimitHi = (limit >> 16) & 0x0F;
+    gdt->flagsLimitHi |= (flags) & 0xF0;
+    gdt->access = access;
 }
 
 uint32_t GDT_getBase(gdt_t *gdt)
@@ -25,14 +29,22 @@ uint32_t GDT_getLimit(gdt_t *gdt)
 
 void GDT_init(void)
 {
-    GDT_create(&GDT[GDT_ENTRY_NULL], 0, 0, 0);
-    GDT_create(&GDT[GDT_ENTRY_UNUSED], 0, 0, 0);
-    GDT_create(&GDT[GDT_ENTRY_CODE], 0, 6*1024*1024, 0x9A);     // ring 0 (kernel), code
-    GDT_create(&GDT[GDT_ENTRY_DATA], 0, 6*1024*1024, 0x92);     // ring 0 (kernel), data
+    GDT_create(&GDT[GDT_ENTRY_NULL], 0, 0, 0, 0);
+    GDT_create(&GDT[GDT_ENTRY_CODE], 0, 0xFFFFFFFF, 0x9A, 0xCF);        // ring 0 (kernel), code
+    GDT_create(&GDT[GDT_ENTRY_DATA], 0, 0xFFFFFFFF, 0x92, 0xCF);        // ring 0 (kernel), data
 
-    gdt_ptr_t gdtr;
-    gdtr.size = (sizeof(GDT) - 1);
-    gdtr.address = &GDT;
+    pGDT.size = (sizeof(GDT) - 1);
+    pGDT.address = (uint32_t)&GDT;
 
-    asm volatile("lgdt (%0)" : : "p" ((uint8_t*)&gdtr));
+    GDT_flush();
+}
+
+uint16_t GDT_codeSegmentSelector(void)
+{
+    return (uint8_t*)&GDT[GDT_ENTRY_CODE] - (uint8_t*)GDT;
+}
+
+uint16_t GDT_dataSegmentSelector(void)
+{
+    return (uint8_t*)&GDT[GDT_ENTRY_DATA] - (uint8_t*)GDT;
 }
--- gdt.h
+++ gdt.h
@@ -8,7 +8,7 @@ typedef struct gdt_s
     uint16_t limitLo;
     uint16_t baseLo;
     uint8_t baseHi;
-    uint8_t type;
+    uint8_t access;
     uint8_t flagsLimitHi;
     uint8_t baseExt;
 } __attribute__((packed)) gdt_t;
@@ -19,20 +19,23 @@ typedef struct gdt_ptr_s
     uint32_t address;
 } __attribute__((packed)) gdt_ptr_t;
 
-void GDT_create(gdt_t *gdt, uint32_t base, uint32_t limit, uint8_t type);
+void GDT_create(gdt_t *gdt, uint32_t base, uint32_t limit, uint8_t access, uint8_t flasg);
 uint32_t GDT_getBase(gdt_t *gdt);
 uint32_t GDT_getLimit(gdt_t *gdt);
 
 
 #define GDT_ENTRY_NULL      0
-#define GDT_ENTRY_UNUSED    1
-#define GDT_ENTRY_CODE      2
-#define GDT_ENTRY_DATA      3
+#define GDT_ENTRY_CODE      1
+#define GDT_ENTRY_DATA      2
 
-#define GDT_ENTRY_COUNT     4
+
+#define GDT_ENTRY_COUNT     3
 
 extern gdt_t GDT[GDT_ENTRY_COUNT];
+extern gdt_ptr_t pGDT;
 
 void GDT_init(void);
+uint16_t GDT_codeSegmentSelector(void);
+uint16_t GDT_dataSegmentSelector(void);
 
 #endif // __GDT_H__
